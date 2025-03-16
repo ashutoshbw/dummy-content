b97b9f66fc88d9e91ea65ef753b1d6f31874788f
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-03-16 11:55:07 +0600

    fix: fix parsing prettified pre tags
    replace node-html-parser with htmlparser2 since node-html-parser is buggy when pre tags are prettified.

--- packages/dummy-content/src/handle-html.ts
+++ packages/dummy-content/src/handle-html.ts
@@ -1,8 +1,8 @@
 import path from 'node:path';
 import { readFile } from './utils';
 import fs from 'node:fs/promises';
-import { parse } from 'node-html-parser';
 import { checkFileExists } from './utils';
+import * as htmlparser2 from 'htmlparser2';
 
 function htmlTemplate(content: string) {
   return `<!doctype html>
@@ -25,24 +25,44 @@ export async function createOrFillHtml(
 
   if (id !== '') {
     const htmlString = await readFile(filePath);
-    const root = parse(htmlString);
 
-    const element = root.getElementById(id);
+    let startIndex: number | null = null;
+    let endIndex: number | null = null;
+    let endTagFound = false;
+    let innerDivs = 0;
+    const parser = new htmlparser2.Parser({
+      onopentag(name, attributes) {
+        if (name === 'div') {
+          if (attributes.id === id) {
+            // parser startIndex and endIndex reference: https://github.com/fb55/htmlparser2/issues/1224#issuecomment-1166629597
+            startIndex = parser.endIndex + 1;
+          } else if (endTagFound == false) {
+            innerDivs++;
+          }
+        }
+      },
+      onclosetag(tagname) {
+        if (tagname === 'div') {
+          if (startIndex !== null && endTagFound == false) {
+            if (innerDivs === 0) {
+              endIndex = parser.startIndex;
+              endTagFound = true;
+            } else innerDivs--;
+          }
+        }
+      },
+    });
+    parser.write(htmlString);
+    parser.end();
 
-    if (element === null) {
+    if (startIndex !== null && endIndex !== null) {
+      const result =
+        htmlString.slice(0, startIndex) + content + htmlString.slice(endIndex);
+      await fs.writeFile(filePath, result);
+      console.log(`${filename} is filled`);
+    } else {
       console.warn(`No element found of id '${id}' in '${filename}'`);
-      return;
     }
-
-    element.innerHTML = content;
-
-    const result =
-      htmlString.slice(0, element.range[0]) +
-      element.outerHTML +
-      htmlString.slice(element.range[1]);
-
-    await fs.writeFile(filePath, result);
-    console.log(`${filename} is filled`);
   } else {
     const fileExists = checkFileExists(filePath);
     await fs.writeFile(filePath, htmlTemplate(content));
